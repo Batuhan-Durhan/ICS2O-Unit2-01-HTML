c75507c57a7ebdead4452371d8b95d05384ac076
Batuhan-Durhan <71403385+Batuhan-Durhan@users.noreply.github.com>
2022-03-08 18:41:59 +0000

    added js

---
+++ script.js
@@ -0,0 +1,11 @@
+// Copyright (c) 2022 Batuhan Durhan All rights reserved
+//
+// Created by: Batuhan Durhan
+// Created on: Mar 2022
+// This file contains the JS functions for index.html
+
+function myButtonClicked() {
+  // this function displays "Hello, World!"
+
+  document.getElementById("hello-world").innerHTML = "<p>Hello, World!</p>";
+}
